c380b8fb1e0e46e0fbbba4abe9515bcd7dc62da7
Leonardo Rodrigues <leordoria2016@gmail.com>
2023-12-08 20:07:53 -0300

    2º Commit
    Auth funcinality - Appwrite

--- src/_auth/forms/SingupForm.tsx
+++ src/_auth/forms/SingupForm.tsx
@@ -17,6 +17,7 @@ import { SignUpValidation } from "@/lib/validation"
 import { z } from "zod"
 import Loader from "@/components/shared/Loader"
 import { Link } from "react-router-dom"
+import { createUserAccount } from "@/lib/appwrite/api"
 
  
 
@@ -34,10 +35,9 @@ const SingupForm = () =>{
       })
      
       // 2. Define a submit handler.
-      function onSubmit(values: z.infer<typeof SignUpValidation>) {
-        // Do something with the form values.
-        // ✅ This will be type-safe and validated.
-        console.log(values)
+      async function onSubmit(values: z.infer<typeof SignUpValidation>) {
+        const newUser = await createUserAccount(values)
+        console.log(newUser)
       }
 
     return(
